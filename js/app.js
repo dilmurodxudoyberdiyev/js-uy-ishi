6840ac45a3f0db35fc0a611f4e11fd545535a9c7
dilmurodxudoyberdiyev <dxudoyberdiyev041@gmail.com>
2022-10-28 11:43:57 +0500

    js uy ishi

---
+++ js/app.js
@@ -0,0 +1,206 @@
+// 1 misol 
+
+// let a = +prompt("birinchi sonni kiriting");
+// let b = prompt("amalni kiriting");
+// let c = +prompt("ikkinchi sonni kiriting");
+
+// switch (b){
+//     case"+":
+//     natija = a + c;
+//     break
+   
+//     case"-":
+//     natija = a - c;
+//     break
+  
+//     case"*":
+//     natija = a * c;
+//     break
+    
+//     case"/":
+//     natija = a / c;
+//     break
+
+//     case"**":
+//     natija = a**c;
+//     break
+
+// }
+
+// console.log(natija);
+
+
+// 2 misol 
+
+// let numbers = [ 3.02, -3.65, 5, -9];
+
+// normalize(numbers)
+
+// function normalize(nums){
+//     let arr = []
+//     for(let i = 0; i < nums.length; i++){
+//         if(nums[i]<0){
+//             let son = Math.round(nums[i]) * -1
+//             arr.push(son)
+//         }
+//         else{
+//             let son= Math.round(nums[i])
+//             arr.push(son)
+//         }
+//     }
+//     console.log(arr);
+// }
+
+// normalize(numbers)
+
+
+// 3 misol  k sonini n marta chiqarish.
+
+// let k = 10;
+// let n = 20;
+
+// for(i = 1; i <= n; i++){
+//     console.log(k);
+// }
+
+// 4 misol sonni tub yoki tub emasligini topish.
+
+// let son = 3;
+// let idf = 0;
+
+// for(let i =2; i < son; i++)
+// if(son % i == 0){
+//     idf = idf + 1;
+// }
+
+// idf ? console.log(" son tub emas"): console.log("son tub");
+
+// 5 misol 1000 gacha uchga bo'linadiganlar sonlar.
+
+// function uch(num){
+//     for(i = 1; i <= num; i++){
+//         if(i % 3 == 0){
+//             console.log(i);
+//         }
+//     }
+// }
+
+// uch(2000);
+
+
+// 6 misol 2 ta sonni funksiya orqali bo'lish
+
+// let num1 = Number(prompt("birinchi sonni kiriting"));
+// let num2 = Number(prompt("ikkinchi sonni kiriting"));
+
+// function bulish(num1, num2){
+//     return num1 / num2;
+// }
+// natija = bulish(num1 / num2);
+// console.log(natija);
+
+// 7 misol 2 ta sonni funksiya orqali ko'paytirish
+
+// let num1 = Number(prompt("birinchi sonni kiriting"));
+// let num2 = Number(prompt("ikkinchi sonni kiriting"));
+
+// function kupaytirish(num1, num2){
+//     return num1 * num2;
+// }
+// natija = kupaytirish(num1 * num2);
+// console.log(natija);
+
+
+// 8 misol 1000 gacha toq sonlarni yig'indisini chiqarish
+
+// let summ = 0;
+// function toqSumm(son){
+//     for(i = 1; i <= son; i++){
+//         if(i % 2 == 1){
+//             summ = summ + i;
+
+//         }
+//     }
+//     console.log(summ);
+// }
+
+// toqSumm(1000);
+
+// 9 misol ikkta sonni yig'indisini chqaruvchi dastur tuzish.
+
+// const abb = function(a,b){
+//     return a + b;
+// }
+
+// let c = abb(49,6)
+
+// console.log(c);
+
+
+// 10 misol butun sonlarni tub yoki tub emasligini aniqlovch funksiya yarating
+
+// let son = +prompt("sonni kiriting")
+
+// function func(son){
+//     let result = 0
+//     for(i = 1; i <= son; i++){
+//         if(son % i == 0){
+//             result = result + 1;
+
+//         }
+//     }
+//     if (result==2){
+//         console.log("tub son");
+//     }
+//     else{
+//         console.log("tub son emas");
+//     }
+// }
+
+// func(son);
+
+// 11 misol 
+
+// let a = [12,5,4,5,6,9,16,117,100,102,1];
+// let b = 3;
+// let arr = [];
+
+// function func(a,b){
+//     for(let i=1; i<a.length; i++){
+//         a[i]=a[i]+b;
+//         arr.push(a[i])
+//     }
+//     console.log(arr);
+// }
+
+
+// func(a,b);
+
+// 12 misol #43
+
+
+// let a = [ 12,3,4,6,];
+// let b = 0;
+// function mean(a){
+//     for(let i=1; i<a.length; i++){
+//         b=b+a[i];
+//     }
+//     console.log(b/a.length);
+// }
+
+// mean(a);
+
+// 13 misol
+
+
+
+
+
+
+
+
+
+
+
+
+
